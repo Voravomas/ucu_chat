5d3c258fd6c586b99f4746d6fb017a3bf748c115
Marki <lebyak.m@gmail.com>
2021-06-23 23:51:40 +0300

    Designing chat page

--- lib/net/api_methods.dart
+++ lib/net/api_methods.dart
@@ -1,2 +1,1 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:ucuchat/model/user.dart';
--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,18 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:ucuchat/constants.dart';
+import 'package:ucuchat/models/message_model.dart';
 import 'package:ucuchat/utils.dart';
 
 class Chat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        // appBar: AppBar(
-        //   title: Text('UCU Chat'),
-        // ),
-        body: Column(
-      children: [
-        getText("Mykyta [20:23]: Hello World!"),
-        Center(child: getMockedbutton(context, 'Send')),
-      ],
+    return Expanded(
+        child: Container(
+      child: ListView.builder(
+        itemCount: chats.length,
+        itemBuilder: (BuildContext context, int index) {
+          final Message chat = chats[index];
+          return Container(
+            margin: EdgeInsets.only(top: 2.5, bottom: 2.5, right: 5.0),
+            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            decoration: BoxDecoration(
+              color: Color(0xffebe9e6),
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(20.0),
+                bottomRight: Radius.circular(20.0),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 35.0,
+                      backgroundImage: AssetImage(chat.sender.imageUrl),
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          chat.sender.name,
+                          style: TextStyle(
+                            color: primaryColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5.0,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.45,
+                          child: Text(
+                            chat.text,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                              // fontSize: 13.0,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      chat.time,
+                      style: TextStyle(
+                        color: primaryColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     ));
   }
 }
