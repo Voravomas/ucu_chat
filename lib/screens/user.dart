a7993dc1395acc72699f28275c94357060b3f818
Kate Detsyk <detsyk.k@ucu.edu.ua>
2021-06-27 23:16:53 +0300

    user profile

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -1,29 +1,26 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:ucuchat/constants.dart';
+import 'package:ucuchat/net/api_methods.dart';
+import 'package:ucuchat/screens/profile_settings.dart';
 import 'package:ucuchat/utils.dart';
 
-const const_name = "Abraham Lincoln";
-const const_email = "lincoln@ucu.edu.ua";
-const const_phone = "+380123456789";
-
-Container getTopText(text) {
-  return Container(
-    margin: const EdgeInsets.only(top: 80.0, bottom: 120.0),
-    child: Text(
-      text,
-      style: AppTextStyles.robotoRed21Bold,
-    ),
-  );
-}
 
 Container getPairText(key, value) {
   return Container(
     child: Column(
       children: [
         Text(key, style: AppTextStyles.robotoRed18Bold),
+        SizedBox(height: 3,),
         Text(value, style: AppTextStyles.robotoBlack18Reg),
+        Divider(thickness: 1,),
       ],
     ),
-    margin: EdgeInsets.only(top: 20.0),
+    margin: EdgeInsets.only(top: 10.0,),
   );
 }
 
@@ -39,23 +36,164 @@ Container getUserInfoText(name, email, phone) {
   );
 }
 
-class UserPage extends StatelessWidget {
+class Avatar extends StatelessWidget {
+  final String? imgUrl;
+  const Avatar({Key? key, required this.imgUrl}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (imgUrl == null) {
+      return CircleAvatar( backgroundImage: AssetImage(default_user_logo));
+    }
+    return CircleAvatar( backgroundImage: NetworkImage(imgUrl!));
+  }
+}
+
+class UserData extends StatelessWidget {
+  final DocumentSnapshot? textFromDB;
+  const UserData({Key? key, required this.textFromDB}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (textFromDB == null) {
+      return Container(child: Padding(padding: EdgeInsets.only(top: 100.0),
+          child: CircularProgressIndicator(color: primaryColor,)));
+    }
+    return Container(
+        child: getUserInfoText(textFromDB!['name'],
+                               textFromDB!['email'],
+                               textFromDB!['phone'])
+    );
+  }
+}
+
+class UserPage extends StatefulWidget {
+  const UserPage({Key? key}) : super(key: key);
+
+  @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  final ImagePicker _picker = ImagePicker();
+
+  String? _imgUrl;
+  DocumentSnapshot? _textFromDB;
+
+  @override
+  void initState() {
+    super.initState();
+    avatarState();
+    userDataState();
+  }
+
+  avatarState() {
+    getDataFromFirestore().then((val) => setState(() {
+      _imgUrl = val['imgUrl'];
+    }));
+  }
+
+  userDataState() {
+    getDataFromFirestore().then((val) => setState(() {
+      _textFromDB = val;
+    }));
+  }
+
+  _uploadImageToDB(image, uid) {
+    final FirebaseStorage storage = FirebaseStorage.instance;
+
+    Reference ref = storage.ref().child('user_profile/${uid}');
+    UploadTask uploadTask = ref.putFile(File(image!.path));
+
+
+    uploadTask.whenComplete(() async {
+      var userLogoUrl = await ref.getDownloadURL();
+
+      FirebaseFirestore.instance.collection('users')
+          .doc(uid).update({"imgUrl": userLogoUrl}).then((_) => avatarState());
+    }).catchError((onError) {
+      print(onError);
+    });
+  }
+
+  _editAvatarButton() {
+    return SizedBox(
+      height: 50, width: 50,
+      child: TextButton(
+          child: Icon(Icons.edit_rounded, color: Colors.white,),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(primaryColor),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                    side: BorderSide(color: Colors.white, width: 2.0))
+            ),),
+          onPressed: () async {
+            PickedFile? image = await _picker.getImage(source: ImageSource.gallery);
+            _uploadImageToDB(image, getCurrentUserId());
+          }),
+    );
+  }
+
+  _footerButtons() {
+    return Container(
+        color: Color(0xfff0e9df),
+        child: Row (
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(
+                onPressed: (){changeLoginAndPasswordPopUp(context);},
+                child: Row( children: [
+                  Text('Change Login and Password'),
+                  SizedBox(width: 3.0,),
+                  Icon(Icons.vpn_key)
+                ])
+            ),
+            TextButton(
+                onPressed: (){changeSettingsPopUp(context, userDataState);},
+                child: Row(children: [
+                  Text('Edit Profile'),
+                  SizedBox(width: 3.0,),
+                  Icon(Icons.settings)
+                ],)
+            ),
+          ],
+        )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(
-        //   title: getAppBarText('UCU Chat'),
-        // ),
-        body: Column(
-      children: [
-        getTopText("User Page"),
-        getUserInfoText(const_name, const_email, const_phone),
-        Center(
-            child: Container(
-          // child: getRedbutton(context, 'Back to chats', 'pop'),
-          padding: EdgeInsets.only(top: 150.0),
-        )),
-      ],
-    ));
+      body: Column(
+        children: [
+          // getTopText("User Page"),
+          SizedBox(height: 30,),
+          SizedBox(
+            height: 200, width: 200,
+            child: Stack(
+              fit: StackFit.expand,
+              clipBehavior: Clip.none,
+              children: [
+                Avatar(imgUrl: _imgUrl,),
+                Positioned(
+                  right: 0, bottom: 0,
+                  child: _editAvatarButton(),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20.0,),
+          UserData(textFromDB: _textFromDB,),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(0.0),
+        child: _footerButtons(),
+      ),
+    );
   }
 }
+
+
+
